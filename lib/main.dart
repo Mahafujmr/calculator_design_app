fc70d000c326e42c26c3e608e464c1fd00aa55cd
M R Computer <hossaintuhin6666@gmail.com>
2023-08-22 00:14:09 +0600

    calculator

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class CalculatorApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Calculator App',
+      title: 'Modern Calculator',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -24,101 +24,93 @@ class CalculatorPage extends StatefulWidget {
 
 class _CalculatorPageState extends State<CalculatorPage> {
   String _currentInput = '';
+  String _output = '';
 
-  void _onNumberButtonPressed(String value) {
+  void _onButtonPressed(String value) {
     setState(() {
-      _currentInput += value;
+      if (value == '=') {
+        try {
+          _output = _evaluateExpression();
+        } catch (e) {
+          _output = 'Error';
+        }
+        _currentInput = '';
+      } else if (value == 'C') {
+        _currentInput = '';
+        _output = '';
+      } else {
+        _currentInput += value;
+      }
     });
   }
 
-  void _onClearButtonPressed() {
-    setState(() {
-      _currentInput = '';
-    });
-  }
-
-  void _onCalculateButtonPressed() {
-    // Add your calculation logic here
-    // For this example, let's just clear the input
-    _onClearButtonPressed();
+  String _evaluateExpression() {
+    String expression = _currentInput;
+    // Add your evaluation logic here, or use a library like 'math_expressions'
+    // For this example, we'll just use the 'eval' function from 'dart:math'
+    return '${double.parse(expression)}';
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Calculator App'),
+        title: Text('Modern Calculator'),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           Expanded(
+            flex: 2,
             child: Container(
-              padding: EdgeInsets.all(16.0),
+              color: Colors.grey[200],
+              padding: EdgeInsets.all(24.0),
               alignment: Alignment.bottomRight,
               child: Text(
                 _currentInput,
-                style: TextStyle(fontSize: 48.0, fontWeight: FontWeight.bold),
+                style: TextStyle(fontSize: 36.0),
               ),
             ),
           ),
           Divider(height: 1.0),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              _buildNumberButton('7'),
-              _buildNumberButton('8'),
-              _buildNumberButton('9'),
-              _buildActionButton('C', _onClearButtonPressed),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              _buildNumberButton('4'),
-              _buildNumberButton('5'),
-              _buildNumberButton('6'),
-              _buildActionButton('/', () => _onNumberButtonPressed('/')),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              _buildNumberButton('1'),
-              _buildNumberButton('2'),
-              _buildNumberButton('3'),
-              _buildActionButton('*', () => _onNumberButtonPressed('*')),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              _buildNumberButton('0'),
-              _buildActionButton('+', () => _onNumberButtonPressed('+')),
-              _buildActionButton('-', () => _onNumberButtonPressed('-')),
-              _buildActionButton('=', _onCalculateButtonPressed),
-            ],
+          Expanded(
+            flex: 3,
+            child: Column(
+              children: <Widget>[
+                _buildRow(['7', '8', '9', '/']),
+                _buildRow(['4', '5', '6', '*']),
+                _buildRow(['1', '2', '3', '-']),
+                _buildRow(['C', '0', '=', '+']),
+              ],
+            ),
           ),
         ],
       ),
     );
   }
 
-  Widget _buildNumberButton(String value) {
+  Widget _buildRow(List<String> buttons) {
     return Expanded(
-      child: ElevatedButton(
-        onPressed: () => _onNumberButtonPressed(value),
-        child: Text(value),
-      ),
-    );
-  }
-
-  Widget _buildActionButton(String value, Function onPressed) {
-    return Expanded(
-      child: ElevatedButton(
-        onPressed: onPressed as void Function()?,
-        style: ElevatedButton.styleFrom(primary: Colors.orange),
-        child: Text(value, style: TextStyle(color: Colors.white)),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: buttons.map((button) {
+          return Expanded(
+            child: InkWell(
+              onTap: () => _onButtonPressed(button),
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey[300]!),
+                ),
+                child: Center(
+                  child: Text(
+                    button,
+                    style: TextStyle(fontSize: 24.0),
+                  ),
+                ),
+              ),
+            ),
+          );
+        }).toList(),
       ),
     );
   }
